3e3ee50a8f4735320ad4fef7b4db9282ff4926be
Waqar M <waqar.mahmood.pro@gmail.com>
2014-04-09 15:27:30 -0500

    First commit

---
+++ public_html/validation.js
@@ -0,0 +1,9 @@
+document.write("<table><tr><td style='font-size:small'>"
++ "<a href=\"http://validator.w3.org/check/referer\"> "
++ "Conformance<br />Checker</a><br />"
++ "<a href=\"http://validator.w3.org/checklink/\" "
++ "onclick=\"window.open(this.href,'_blank');return false;\">Link Checker<a></td>");
+
+document.write("<td><a href=\"http://jigsaw.w3.org/css-validator/check/referer\"> "
++ "<img src=\"http://www.austincc.edu/jscholl/images/vcss.png\" alt=\"Valid CSS!\" height=\"31\" width=\"88\" "
++ "style=\"border: 0px;\" /></a></td></tr></table>")
